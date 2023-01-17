427dd5eb69d0d0e928abd057bd193240341f69d7
DESKTOP-KVCQHCD\SSAFY <godan6869@naver.com>
2023-01-17 16:49:33 +0900

    chore: 글로벌 css 설정 및 리셋

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,18 @@
-const Button = () => {
-  return <div>Button</div>;
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  width: 100px;
+  height: 100px;
+  background-color: var(--colors-brand-500);
+`;
+
+interface IButtonProps {
+  children: React.ReactNode;
+}
+
+const Button = ({ children }: IButtonProps) => {
+  return <StyledButton>{children}</StyledButton>;
 };
 
 export default Button;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import router from './router';
+import GlobalStyles from './styles/GlobalStyles';
+import { lightTheme } from './styles/theme';
+import './styles/variable.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GlobalStyles />
+    <RecoilRoot>
+      <ThemeProvider theme={lightTheme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
