e2a851ed502060264c8799f882f69ae0559e37f0
nimusmix <nimusmix@gmail.com>
2023-01-19 00:27:24 +0900

    feat: div 컴포넌트 구현

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Button from './button/Button';
 import Checkbox from './checkbox/Checkbox';
+import Div from './div/Div';
 import Dropdown from './dropdown/Dropdown';
 import MiniTitle from './font/MiniTitle';
 import Paragraph from './font/Paragraph';
@@ -9,4 +10,4 @@ import Input from './input/Input';
 import Li from './list/Li';
 import Ul from './list/Ul';
 
-export { Button, Input, Checkbox, Ul, Li, Dropdown, Title, SubTitle, MiniTitle, Paragraph };
+export { Button, Div, Input, Checkbox, Ul, Li, Dropdown, Title, SubTitle, MiniTitle, Paragraph };
