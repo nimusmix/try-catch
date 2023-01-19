1aec5934c9f53a764b3db4e08e98a03e071ef925
hyeonaseodev <hyeona.seo.dev@gmail.com>
2023-01-20 05:22:21 +0900

    feat: MainCarousel 구현 완료

--- src/components/carousel/MainCarousel.tsx
+++ src/components/carousel/MainCarousel.tsx
@@ -1,33 +1,79 @@
 import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import './MainCarousel.css';
 
 interface ICarouselProps {
-  items: { id: number; bgcolor: string }[];
+  items: { id: number; bgColor: string; children: React.ReactNode }[];
+  active: string;
+  bgColor: string;
+  transform: string;
 }
 
-// const colors: { id: number; bgcolor: string }[] = [
-//   { id: 0, bgcolor: '#0088FE' },
-//   { id: 1, bgcolor: '#00C49F' },
-//   { id: 2, bgcolor: '#FFBB28' },
-// ];
-
-const colors: { id: number; bgcolor: string }[] = [
-  { id: 0, bgcolor: '#1' },
-  { id: 1, bgcolor: '#2' },
-  { id: 2, bgcolor: '#3' },
+const colors: { id: number; bgColor: string; children: React.ReactNode }[] = [
+  {
+    id: 0,
+    bgColor: 'var(--colors-brand-100)',
+    children: (
+      <div
+        style={{
+          position: 'relative',
+          top: '80%',
+          left: '60%',
+          display: 'flex',
+          alignItems: 'center',
+          padding: '0 1.25rem 1.875rem 3.75rem',
+          fontSize: 'var(--fonts-desktop-heading-2xl)',
+          lineHeight: 'var(--lineHights-desktop-heading-2xl)',
+        }}
+      >
+        트라이 캐치가 분석한 2022 기술 동향레포트
+      </div>
+    ),
+  },
+  { id: 1, bgColor: '#EAFDFC', children: <div>트라이 캐치2</div> },
+  { id: 2, bgColor: '#EEF1FF', children: <div>트라이 캐치3</div> },
 ];
-// const colors = ['#0088FE', '#00C49F', '#FFBB28'];
 
 const delay = 2500;
 
 const SlideshowWrapper = styled.div`
   margin: 0 auto;
   overflow: hidden;
-  max-width: 500px;
 `;
 
-const Slideshow = (items: ICarouselProps) => {
+const SlideshowSlider = styled.div<Partial<ICarouselProps>>`
+  white-space: nowrap;
+  transition: ease 1000ms;
+  transform: ${({ transform }) => transform};
+`;
+
+const Slide = styled.div<Partial<ICarouselProps>>`
+  display: inline-block;
+
+  height: 400px;
+  width: 100%;
+  background-color: ${({ bgColor }) => bgColor || 'red'};
+`;
+
+const SlideshowDots = styled.div`
+  position: relative;
+  bottom: 50px;
+  text-align: center;
+`;
+
+const SlideshowDot = styled.div<Partial<ICarouselProps>>`
+  display: inline-block;
+  height: 16px;
+  width: 16px;
+  border-radius: var(--borders-radius-round);
+
+  cursor: pointer;
+  margin: 15px 7px 0px;
+
+  background-color: ${({ active }) =>
+    active === 'active' ? 'var(--colors-brand-500)' : 'var(--colors-white-300)'};
+`;
+
+const Slideshow = () => {
   const [index, setIndex] = useState(0);
   const timeoutRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
@@ -50,27 +96,27 @@ const Slideshow = (items: ICarouselProps) => {
   }, [index]);
 
   return (
-    <div className="slideshow">
-      <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+    <SlideshowWrapper>
+      <SlideshowSlider transform={`translate3d(${-index * 100}%, 0, 0)`}>
         {colors.map((item) => (
-          <div className="slide" key={item.id} style={{ border: '1px solid' }}>
-            {item.bgcolor}{' '}
-          </div>
+          <Slide key={item.id} bgColor={item.bgColor}>
+            {item.children}
+          </Slide>
         ))}
-      </div>
+      </SlideshowSlider>
 
-      {/* <div className="slideshowDots">
+      <SlideshowDots>
         {colors.map((item) => (
-          <div
+          <SlideshowDot
             key={item.id}
-            className={`slideshowDot${index === item.id ? ' active' : ''}`}
+            active={`${index === item.id ? 'active' : ''}`}
             onClick={() => {
               setIndex(item.id);
             }}
           />
         ))}
-      </div> */}
-    </div>
+      </SlideshowDots>
+    </SlideshowWrapper>
   );
 };
 
