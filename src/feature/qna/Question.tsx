d11b19ed2cf58cab9bc2d1a2b2ca9d6b6815ad62
nimusmix <nimusmix@gmail.com>
2023-01-23 17:06:47 +0900

    feat: Question Feature 완성

--- src/feature/qna/Question.tsx
+++ src/feature/qna/Question.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import { BsCheckCircleFill } from 'react-icons/bs';
+import { RxBookmark, RxBookmarkFilled } from 'react-icons/rx';
+import { BsCheckCircleFill, BsShareFill } from 'react-icons/bs';
+import { AiFillLike, AiOutlineLike } from 'react-icons/ai';
 import { Button, Div, MiniTitle, Paragraph } from '../../components';
 import elapsedTime from '../../utils/elapsed-time';
 
@@ -28,11 +30,25 @@ const QuestionDiv = styled(Div)`
   justify-content: start;
 `;
 
+const UpperWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const UpperTagWrapper = styled.div`
   display: flex;
 `;
 
 const UpperTag = styled(Button)``;
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    margin-left: 0.6rem;
+    cursor: pointer;
+  }
+`;
 
 const QuestionInfoWrapper = styled.div`
   display: flex;
@@ -56,8 +72,24 @@ const CompanyImg = styled.img`
 `;
 
 const Line = styled.div`
-  margin: 1rem 0;
+  margin-top: 1rem;
+  margin-bottom: 1.6rem;
   border-bottom: 0.8px ${({ theme }) => theme.borderColor} solid;
+`;
+
+const Like = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 1.6rem;
+  margin-left: auto;
+  margin-right: auto;
+  svg {
+    margin-right: 0.2rem;
+    color: ${({ theme }) => theme.textColor100};
+  }
+  p {
+    margin-top: 0.1rem;
+  }
 `;
 
 const Question = () => {
@@ -76,39 +108,51 @@ const Question = () => {
     tags: ['react', 'react-hook-form'],
     likeCount: 3,
     answerCount: 12,
-    timestamp: 1674183600,
-    isLiked: true,
+    timestamp: 174183600,
+    isLiked: false,
     isSolved: true,
     isBookMarked: false,
   };
 
   return (
     <QuestionDiv width="820px" padding="2rem">
-      <UpperTagWrapper>
-        {/* 카테고리 */}
-        <UpperTag
-          designType="purpleFill"
-          fontSize="14px"
-          padding="0.2rem 0.6rem"
-          borderRadius="10px"
-        >
-          {MQuestion.category}
-        </UpperTag>
-
-        {/* 해결 여부 */}
-        {MQuestion.isSolved && (
+      <UpperWrapper>
+        <UpperTagWrapper>
+          {/* 카테고리 */}
           <UpperTag
-            designType="greenFill"
+            designType="purpleFill"
             fontSize="14px"
             padding="0.2rem 0.6rem"
-            margin="0 0 0 0.4rem"
             borderRadius="10px"
           >
-            <BsCheckCircleFill size="14" className="solved-icon" />
-            &nbsp;Catched
+            {MQuestion.category}
           </UpperTag>
-        )}
-      </UpperTagWrapper>
+
+          {/* 해결 여부 */}
+          {MQuestion.isSolved && (
+            <UpperTag
+              designType="greenFill"
+              fontSize="14px"
+              padding="0.2rem 0.6rem"
+              margin="0 0 0 0.4rem"
+              borderRadius="10px"
+            >
+              <BsCheckCircleFill size="14" className="solved-icon" />
+              &nbsp;Catched
+            </UpperTag>
+          )}
+        </UpperTagWrapper>
+
+        <Icons>
+          {/* 북마크 */}
+          {MQuestion.isBookMarked && <RxBookmarkFilled size="20" color="var(--colors-brand-500)" />}
+          {MQuestion.isBookMarked || <RxBookmark size="20" color="var(--colors-brand-500)" />}
+
+          {/* 공유 */}
+          <BsShareFill size="16" color="var(--colors-brand-500)" />
+        </Icons>
+      </UpperWrapper>
+
       <MiniTitle sizeType="2xl" textAlign="left" margin="1rem 0 0.6rem 0" fontWeight="600">
         {MQuestion.title}
       </MiniTitle>
@@ -127,6 +171,12 @@ const Question = () => {
       <Text sizeType="base" color="var(--colors-black-100)">
         {MQuestion.content}
       </Text>
+
+      <Like>
+        {MQuestion.isLiked && <AiFillLike />}
+        {MQuestion.isLiked || <AiOutlineLike />}
+        <Text sizeType="xm">{MQuestion.likeCount}</Text>
+      </Like>
     </QuestionDiv>
   );
 };
