d4ac0705214f023d8f597de79e26f82cd822bfd8
nimusmix <nimusmix@gmail.com>
2023-01-23 19:27:02 +0900

    feat: Q&A Detail Page

--- src/pages/challenges/ChallengesPage.tsx
+++ src/pages/challenges/ChallengesPage.tsx
@@ -1,14 +1,10 @@
 import Layout from '../../layout/Layout';
-import { Question, Answer } from '../../feature/qna';
 import { Title } from '../../components';
 
 const ChallengesPage = () => {
   return (
     <Layout>
       <Title>챌린지 메인</Title>
-      <Question />
-      <br />
-      <Answer />
     </Layout>
   );
 };
--- src/pages/qna/QnaDetailPage.tsx
+++ src/pages/qna/QnaDetailPage.tsx
@@ -1,7 +1,16 @@
 import Layout from '../../layout/Layout';
+import { Question, Answer } from '../../feature/qna';
+import { Title } from '../../components';
 
 const QnaDetailPage = () => {
-  return <Layout>QnaDetailPage</Layout>;
+  return (
+    <Layout>
+      <Title>QnaDetailPage</Title>
+      <Question />
+      <br />
+      <Answer />
+    </Layout>
+  );
 };
 
 export default QnaDetailPage;
