510327130cedbb882ebfdb254701c51071327641
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-01-25 05:10:37 +0900

    feat: popular qna 캐러셀 구현 완료

--- src/feature/qna/PopularQna.tsx
+++ src/feature/qna/PopularQna.tsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+import { MiniTitle, Div } from '../../components';
+import StyledSlider from '../../components/carousel/Slider';
+
+const PopularQnaWrapper = styled(Div)`
+  margin-top: 1rem;
+  border-radius: var(--borders-radius-base);
+`;
+
+const PopularQnaTitle = styled(MiniTitle)`
+  font-size: var(--fonts-body-base);
+  line-height: var(--lineHights-body-base);
+  font-weight: 600;
+`;
+
 const PopularQna = () => {
-  return <div>PopularQna</div>;
+  return (
+    <PopularQnaWrapper padding="0rem">
+      <PopularQnaTitle sizeType="xl" textAlign="left" padding="1.25rem 1.625rem 1rem 1.625rem">
+        인기 Q&A
+      </PopularQnaTitle>
+      <StyledSlider />
+    </PopularQnaWrapper>
+  );
 };
 
 export default PopularQna;
