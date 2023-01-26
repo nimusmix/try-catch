e874ffd0bce5ba4aa387f11e63770a7d348cf3b6
June <godan6869@naver.com>
2023-01-27 00:01:49 +0900

    fix: build fix

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -2,11 +2,13 @@ import { TopButton } from '../../components';
 import Layout from '../../layout/Layout';
 import TagList from './tags-loop-slider/TagList';
 import MarqueeLogoCard from './marquee-logo-wall/MarqueeLogoCard';
+import LandingTitle from './landing-title/LandingTitle';
 
 const LandingPage = () => {
   return (
     <Layout>
       <MarqueeLogoCard />
+      <LandingTitle />
       <TagList />
       <TopButton />
     </Layout>
