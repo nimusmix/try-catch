f29ba083908306411b73ec1a437a9f463459b30a
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-02-01 13:49:40 +0900

    style:  Input 컴포넌트 style 변경

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+export type TDesignType = 'borderAll' | 'borderBottom';
 interface IInputProps {
   margin?: string;
   padding?: string;
@@ -9,21 +10,44 @@ interface IInputProps {
   borderRadius?: string;
   fontSize?: string;
   boxShadow?: string;
+  designType?: TDesignType;
 }
+
+const designList = {
+  borderAll: css`
+    border: 1px solid;
+    border-color: ${({ theme: { borderColor } }) => borderColor};
+    border-radius: var(--borders-radius-base);
+  `,
+  borderBottom: css`
+    border-radius: none;
+    border: none;
+    border-bottom: 1px solid;
+    box-shadow: none;
+    border-color: ${({ theme: { borderColor } }) => borderColor};
+  `,
+};
 
 const Input = styled.input<IInputProps>`
   -webkit-appearance: none; /* Safari and Chrome */
   -moz-appearance: none; /* Firefox */
-  appearance: none; /* 네이티브로 지원되는 모양들을 해제하거나 추가 */
+  /* 네이티브로 지원되는 모양들을 해제하거나 추가 */
+  appearance: none;
+
+  ${({ designType }) => designType && designList[designType]};
+  ${({ designType, borderRadius, border }) =>
+    !designType &&
+    css`
+      border: ${border || '1px solid'};
+      border-radius: ${borderRadius || 'var(--borders-radius-base)'};
+      border-color: ${({ theme: { borderColor } }) => borderColor};
+    `};
 
   margin: ${({ margin }) => margin};
   padding: ${({ padding }) => padding || '0.5rem'};
   height: ${({ height }) => height || '2.0rem'};
   width: ${({ width }) => width || '17.875rem'};
-  border: ${({ border }) => border || '1px solid'};
-  border-color: ${({ theme: { borderColor } }) => borderColor};
   box-sizing: border-box;
-  border-radius: ${({ borderRadius }) => borderRadius || 'var(--borders-radius-base)'};
   background-color: ${({ theme: { bgColor } }) => bgColor};
   font-size: ${({ fontSize }) => fontSize || 'var(--fonts-body-base)'};
   color: ${({ theme: { textColor } }) => textColor};
