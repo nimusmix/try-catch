033b40793ec31fcd16ccf882671754c041b59c24
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-02-02 15:12:59 +0900

    feat: feed req, response interface 생성 및 수정

--- src/interface/feed.ts
+++ src/interface/feed.ts
@@ -11,6 +11,131 @@ export interface IFeedItem {
   isBookmarked: boolean; // 북마크 여부
 }
 
+/**
+ * Request get: 피드 검색 요청
+ * {API_URL}/feed/search?
+ *
+ * [] 안의 값이 디폴트
+ *
+ * 본문, 제목 검색
+ * query = [None] | $text
+ *
+ * 정렬
+ * sort = [date] | user
+ *
+ * 구독 필터
+ * subscribe = [false] | true
+ *
+ * 검색 타입
+ * type= [None] | advance
+ *
+ * 날짜 필터
+ * publishDateStart= [None] | 'yyyy-MM-dd' // None: -무한대
+ * publishDateEnd= [None] | 'yyyy-MM-dd' // None: 현재
+ *
+ * 페이지 번호
+ * page= [0] : $number
+ *
+ * 페이지당 불러올 개수
+ * size= [0] : $number
+ */
+
+export interface IFeedSearch {
+  query: string | null;
+  sort: 'date' | 'user';
+  subscribe: boolean;
+  advanced: boolean;
+  publishDateStart: string | null; // 'yyyy-MM-dd';
+  publishDateEnd: string | null; // 'yyyy-MM-dd';
+  page: number; // 0부터 시작 (defualt: 0)
+  size: number; // 페이지당 개수
+}
+
+/**
+ * Response: 피드 관련 응답
+ * {API_URL}/feed/search?
+ * 응답: IFeedList
+ */
+export interface IFeedItemV2 {
+  feedId: string; // 피드 고유 아이디
+  title: string; // 피드 제목
+  summary: string; // 피드 요약 ( 300자 내외 )
+  companyName: string; // 회사 이름
+  logoSrc: string; // 회사 로고 이미지 링크 (현재 로고 이름)
+  createdAt: string; // 피드 생성일 yyyy-MM-dd
+  url: string; // 피드 URL
+  thumbnailImage: string; // 썸네일 이미지 URL
+  tags: Array<string>; // 크롤링 시 수집한 태그
+  keywords: Array<string>; // 인공지능 모델을 통해 추출한 태그
+  isBookmarked: boolean; // 북마크 여부
+}
+
 export interface IFeedList {
   feedList: Array<IFeedItem>;
+}
+
+/**
+ * Request get: Company 추천
+ * {API_URL}/feed/companys?recommend=~
+ *
+ * [] 안의 값이 디폴트
+ *
+ * 정렬: 관심순, 조회순
+ * sort = [viewCount] | recommend
+ *
+ * 불러올 개수
+ * size= [0] | $number
+ */
+interface IFeedCompanyRequest {
+  sort: 'viewCount' | 'recommend';
+  size: number; // 불러올 개수
+}
+
+/**
+ * Response: Company 추천
+ * {API_URL}/feed/companys?recommend=~
+ *
+ * 응답:
+ * Array<IfeedCompany>
+ */
+interface IfeedCompany {
+  companyId: number; // 회사 아이디 (회사 프로필페이지 이동 위함)
+  logoSrc: string; // 회사 이름 (회사 로고 이미지 가져오기 위함)
+  companyName: string; // 회사 이름 표기
+  isFollowed: boolean; // 구독 여부 (비 로그인시: 모두 false)
+  score: number; // 조회수 | 추천 지수
+}
+
+/**
+ * Request post: 피드 Item 북마크
+ * Request put: 피드 Item 북마크 취소
+ * {API_URL}/bookmark
+ *
+ * 헤더 JWT
+ *
+ * body{
+ *    id: number, // '질문아이디' 나 '피드 아이디'
+ *    type: string; // 질문: 'question', 피드: 'feed'
+ * }
+ */
+
+interface IBookmark {
+  id: string;
+  type: string;
+}
+
+/**
+ * Request post: Company 구독
+ * Request put: Company 구독 취소
+ *
+ * {API_URL}/subscribe
+ *
+ * 헤더 JWT
+ *
+ * body{
+ *    companyId : number; // 회사 고유 아이디
+ * }
+ */
+interface ICompanySubscribe {
+  companyId: number; // 회사 고유 아이디
 }
