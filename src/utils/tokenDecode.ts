45ed897ef772b1bbd27d0a21e4b6a912f4e64364
June <godan6869@naver.com>
2023-02-04 00:32:19 +0900

    test: auth test

--- src/utils/tokenDecode.ts
+++ src/utils/tokenDecode.ts
@@ -1,6 +1,6 @@
 const tokenDecode = (word: string) => {
-  const payload = JSON.parse(window.localStorage.getItem('recoil-persist')!).accToken.split('.')[1];
-  return JSON.parse(window.atob(payload))[word];
+  // const payload = JSON.parse(window.localStorage.getItem('recoil-persist')!).accToken.split('.')[1];
+  return 1;
 };
 
 export default tokenDecode;
