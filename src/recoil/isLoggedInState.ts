aa1e66c270fb77b595a418678ebc0d4b2a34002c
nimusmix <nimusmix@gmail.com>
2023-02-03 10:57:44 +0900

    fix: login state persist로 변경 및 navbar style 수정

--- src/recoil/isLoggedInState.ts
+++ src/recoil/isLoggedInState.ts
@@ -1,8 +1,12 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 const isLoggedInState = atom({
   key: 'isLoggedIn',
-  default: false, // 잠시 수정
+  default: false,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export default isLoggedInState;
