348903ac7bdddf29caf267d9279c52c64c58dca2
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-02-05 20:41:14 +0900

    feat: home page layout 구현

---
+++ src/feature/home/index.tsx
@@ -0,0 +1,5 @@
+import WaitForAnswer from './WaitForAnswer';
+import TodayHot from './TodayHot';
+import HallOfFame from './HallOfFame';
+
+export { WaitForAnswer, TodayHot, HallOfFame };
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,30 @@
+import styled from 'styled-components';
 import { Slideshow } from '../../components';
+import { HallOfFame, WaitForAnswer, TodayHot } from '../../feature/home';
 import Layout from '../../layout/Layout';
+
+const HomeBodySection = styled.section`
+  display: flex;
+  flex-direction: row;
+  margin-top: 4.5rem;
+`;
+
+const RightBodySection = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const HomePage = () => {
   return (
     <Layout>
       <Slideshow />
+      <HomeBodySection>
+        <WaitForAnswer />
+        <RightBodySection>
+          <TodayHot />
+          <HallOfFame />
+        </RightBodySection>
+      </HomeBodySection>
     </Layout>
   );
 };
