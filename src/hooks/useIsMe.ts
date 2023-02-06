58fa4ab8359c6f908b3cc5e6321510529a5e5139
DESKTOP-KVCQHCD\SSAFY <godan6869@naver.com>
2023-02-06 15:21:13 +0900

    test: 내가 쓴 글인지 판별하는 코드

--- src/hooks/useIsMe.ts
+++ src/hooks/useIsMe.ts
@@ -1,13 +1,18 @@
 import { useRecoilValue } from 'recoil';
 import { accToken } from '../recoil';
 import tokenDecode from '../utils/tokenDecode';
+import { logOnDev } from '../utils/logging';
 
 const UseIsMe = (userId: number) => {
   const acc = useRecoilValue(accToken);
+  logOnDev.log(userId);
   if (acc) {
     const myId = tokenDecode(acc, 'id');
+    logOnDev.log(myId);
+
     return userId === myId;
   }
+  logOnDev.log(acc);
   return false;
 };
 
