a136d884ffbcd97c08c24e9369b0ab02d1f2d97b
hyeonaseodev <hyeona.seo.dev@gmail.com>
2023-02-08 08:17:45 +0900

    style:  챌린지 아코디언으로 변경

--- src/pages/challenges/ChallengesDetailPage.tsx
+++ src/pages/challenges/ChallengesDetailPage.tsx
@@ -1,7 +1,12 @@
+import OngoingChallengeVer2 from '../../feature/challenge/OngoingChallengeVer2';
 import Layout from '../../layout/Layout';
 
 const ChallengesDetailPage = () => {
-  return <Layout>ChallengesDetailPage</Layout>;
+  return (
+    <Layout>
+      <OngoingChallengeVer2 />
+    </Layout>
+  );
 };
 
 export default ChallengesDetailPage;
