6f3e6c4abfdcac2f33c6b99a07eea3a4cfb096d0
nimusmix <nimusmix@gmail.com>
2023-02-10 09:37:45 +0900

    test: 프로필 모달 팔로워 테스트

--- src/pages/profile/FollowersPage.tsx
+++ src/pages/profile/FollowersPage.tsx
@@ -1,12 +1,28 @@
 import { useQuery } from 'react-query';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ModalWrapper, NavWrapper, NavItem, ItemWrapper } from './SubscriptionPage';
+import { getUserId, getUserFollow } from '../../apis/profile/profile';
+import { ISimpleUserData } from '../../interface/user';
+import SimpleUserItem from '../../feature/user/profile/SimpleUserItem';
 
 const FollowersPage = () => {
   const { userName } = useParams();
-
   const navi = useNavigate();
 
+  const { data: userId, isLoading: userIdLoading } = useQuery<number>(
+    ['myAnswerList', 'userId'] as const,
+    () => getUserId(userName!)
+  );
+  const { data: followers, isLoading: contentLoading } = useQuery<any>(
+    ['user', 'follower'],
+    () => getUserFollow(userId!, { type: 'follower' }),
+    { enabled: !!userId }
+  );
+
+  if (userIdLoading || contentLoading) {
+    return <p>Loading...</p>;
+  }
+  console.logI(followers);
   return (
     <ModalWrapper>
       <NavWrapper>
@@ -18,17 +34,11 @@ const FollowersPage = () => {
         </NavItem>
         <NavItem toggle>팔로워</NavItem>
       </NavWrapper>
+
       <ItemWrapper>
-        <p>{userName}</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
-        <p>팔로워</p>
+        {followers?.map((user: ISimpleUserData) => {
+          return <SimpleUserItem {...user} key={user.userId} />;
+        })}
       </ItemWrapper>
     </ModalWrapper>
   );
