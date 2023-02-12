005fa44cf385f3ea32cea0dfa93ff494c26edee8
June <godan6869@naver.com>
2023-02-12 17:00:18 +0900

    fix: 알림 useEffect 디펜던시 변경

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 import Navigation from './header/Navigation';
 import { TopButton } from '../components';
 import useNotifications from '../hooks/useNotifications';
 import { logOnDev } from '../utils/logging';
+import { accToken, isLoggedInState } from '../recoil';
+import { API_URL } from '../constant';
 
 export interface ILayoutProps {
   children: React.ReactNode;
@@ -18,10 +21,21 @@ export const Main = styled.main`
 `;
 
 const Layout = ({ children }: ILayoutProps) => {
-  const notifications = useNotifications(children);
+  const [sseUrl, setSseUrl] = useState('');
+  const isLoggined = useRecoilValue(isLoggedInState);
+  const acc = useRecoilValue(accToken);
+
+  useEffect(() => {
+    const BASE_URL = `https://${API_URL}/v1`;
+    setSseUrl(`${BASE_URL}/connect?token=${acc}`);
+  }, [acc]);
+
+  const notifications = useNotifications(sseUrl, isLoggined);
+
   useEffect(() => {
     logOnDev.log(notifications);
   }, [notifications, notifications.length, children]);
+
   return (
     <>
       <Navigation />
