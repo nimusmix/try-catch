9ec67635b6c0dc17b4bff386153ac3e01fe89952
nimusmix <nimusmix@gmail.com>
2023-02-13 00:18:37 +0900

    fix: 로드맵 페이지 api 요청 위치 수정

--- src/pages/roadmap/RoadmapEditPage.tsx
+++ src/pages/roadmap/RoadmapEditPage.tsx
@@ -1,25 +1,10 @@
-import styled from 'styled-components';
 import { ReactFlowProvider } from 'reactflow';
-import { useQuery } from 'react-query';
 import { Layout } from '../../layout';
 import { SubTitle, Paragraph } from '../../components';
 import RoadmapForm from '../../feature/roadmap/RoadmapForm';
 import { RoadmapFormWrapper as EditPageWrapper, TitleWrapper } from './RoadmapFormPage';
-import { getName } from '../../apis/auth/auth';
-import { getRoadmapDetail } from '../../apis/roadmap/roadmap';
-import { IRoadmap } from '../../interface/roadmap';
 
 const RoadmapEditPage = () => {
-  const { data: myName } = useQuery<string>(['myName'], () => getName());
-  const { data: roadmapDetail, isLoading } = useQuery<IRoadmap>(
-    ['roadmap', myName] as const,
-    () => getRoadmapDetail(myName!),
-    { enabled: !!myName }
-  );
-
-  console.log('로드맵 디테일!!!', roadmapDetail);
-  console.log(roadmapDetail?.title);
-  console.log(roadmapDetail?.tag);
   return (
     <Layout>
       <EditPageWrapper>
@@ -29,12 +14,7 @@ const RoadmapEditPage = () => {
         </TitleWrapper>
 
         <ReactFlowProvider>
-          <RoadmapForm
-            title={roadmapDetail!.title}
-            tag={roadmapDetail!.tag}
-            nodes={roadmapDetail!.nodes}
-            edges={roadmapDetail!.edges}
-          />
+          <RoadmapForm />
         </ReactFlowProvider>
       </EditPageWrapper>
     </Layout>
