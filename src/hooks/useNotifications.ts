75ad2c7b440d2bc1e292c663f3d2e8522c99c446
June <godan6869@naver.com>
2023-02-12 12:12:30 +0900

    test: 프론트 알림기능 테스트

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -1,57 +1,85 @@
 import { useEffect, useState } from 'react';
-import { sseEvents } from '../utils/sse';
+import { useRecoilValue } from 'recoil';
+import { EventSourcePolyfill } from 'event-source-polyfill';
 import { logOnDev } from '../utils/logging';
+import { accToken, isLoggedInState } from '../recoil';
+import { API_URL } from '../constant';
 
 const useNotifications = () => {
+  const isLoggined = useRecoilValue(isLoggedInState);
+  const acc = useRecoilValue(accToken);
   const [followNotifications, setFollowNotifications] = useState([]);
   const [answerAcceptanceNotifications, setAnswerAcceptanceNotifications] = useState([]);
   const [answerRegistrationNotifications, setAnswerRegistrationNotifications] = useState([]);
 
+  const BASE_URL = `https://${API_URL}/v1`;
+  const notificationURL = `${BASE_URL}/connect`;
+
+  // eslint-disable-next-line consistent-return
   useEffect(() => {
-    // connection 되면
-    sseEvents.addEventListener('open', (e) => {
-      logOnDev.dir(e);
-    });
-    // error 발생시
-    sseEvents.addEventListener('error', (e) => {
-      logOnDev.log(e);
-    });
+    if (isLoggined) {
+      let sseEvents: EventSourcePolyfill;
+      const fetchSSE = async () => {
+        try {
+          // 이벤트를 전달 받기 위해서 서버로 접속을 시작하려면 우선, 이벤트를 생성하는 서버측 스크립트를 URI로 지정하여 새로운 EventSource 객체를 생성한다
+          // EventSource 생성자에 전달 된 URL이 절대 URL 인 경우 해당 출처 (scheme, domain, port)가 호출 페이지의 출처와 일치해야 한다.
+          sseEvents = new EventSourcePolyfill(notificationURL, {
+            headers: {
+              Authorization: acc,
+            },
+            withCredentials: true,
+          });
+          // connection 되면
+          sseEvents.addEventListener('open', (e) => {
+            logOnDev.dir(e);
+          });
+          // error 발생시
+          sseEvents.addEventListener('error', (e) => {
+            logOnDev.log(e);
+          });
 
-    sseEvents.addEventListener('message', (e) => {
-      const data = JSON.parse(e.data);
-      // switch (data.type) {
-      //   // 팔로우
-      //   case 'follow':
-      //     setFollowNotifications((prevFollowNotifications) => [
-      //       ...prevFollowNotifications,
-      //       data.message,
-      //     ]);
-      //     break;
-      //
-      //   // 답변 채택
-      //   case 'answer_acceptance':
-      //     setAnswerAcceptanceNotifications((prevAnswerAcceptanceNotifications) => [
-      //       ...prevAnswerAcceptanceNotifications,
-      //       data.message,
-      //     ]);
-      //     break;
-      //
-      //   // 내 글에 답변 등록
-      //   case 'answer_registration':
-      //     setAnswerRegistrationNotifications((prevAnswerRegistrationNotifications) => [
-      //       ...prevAnswerRegistrationNotifications,
-      //       data.message,
-      //     ]);
-      //     break;
-      //   default:
-      //     break;
-      // }
-    });
+          sseEvents.addEventListener('message', (e) => {
+            const data = JSON.parse(e.data);
+            console.log(data);
+            // switch (data.type) {
+            //   // 팔로우
+            //   case 'follow':
+            //     setFollowNotifications((prevFollowNotifications) => [
+            //       ...prevFollowNotifications,
+            //       data.message,
+            //     ]);
+            //     break;
+            //
+            //   // 답변 채택
+            //   case 'answer_acceptance':
+            //     setAnswerAcceptanceNotifications((prevAnswerAcceptanceNotifications) => [
+            //       ...prevAnswerAcceptanceNotifications,
+            //       data.message,
+            //     ]);
+            //     break;
+            //
+            //   // 내 글에 답변 등록
+            //   case 'answer_registration':
+            //     setAnswerRegistrationNotifications((prevAnswerRegistrationNotifications) => [
+            //       ...prevAnswerRegistrationNotifications,
+            //       data.message,
+            //     ]);
+            //     break;
+            //   default:
+            //     break;
+            // }
+          });
+        } catch (error) {
+          logOnDev.log(error);
+        }
+      };
 
-    return () => {
-      sseEvents.close();
-    };
-  }, []);
+      fetchSSE();
+      return () => {
+        sseEvents.close();
+      };
+    }
+  }, [acc, isLoggined, notificationURL]);
 
   return { followNotifications, answerAcceptanceNotifications, answerRegistrationNotifications };
 };
