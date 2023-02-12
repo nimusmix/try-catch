cddf108689f9f9989b1cb5da12b85bbab8357c21
June <godan6869@naver.com>
2023-02-12 13:11:28 +0900

    fix: withCredentials 제거

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -22,9 +22,7 @@ const useNotifications = () => {
         try {
           // 이벤트를 전달 받기 위해서 서버로 접속을 시작하려면 우선, 이벤트를 생성하는 서버측 스크립트를 URI로 지정하여 새로운 EventSource 객체를 생성한다
           // EventSource 생성자에 전달 된 URL이 절대 URL 인 경우 해당 출처 (scheme, domain, port)가 호출 페이지의 출처와 일치해야 한다.
-          sseEvents = new EventSource(notificationURL, {
-            withCredentials: true,
-          });
+          sseEvents = new EventSource(notificationURL);
           // connection 되면
           sseEvents.addEventListener('open', (e) => {
             logOnDev.log('sse 연결됨');
