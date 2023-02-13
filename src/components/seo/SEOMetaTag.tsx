fda9f4a2d12405692c1c2a8cc5538615b6d63896
June <godan6869@naver.com>
2023-02-14 00:14:21 +0900

    fix: SEO

--- src/components/seo/SEOMetaTag.tsx
+++ src/components/seo/SEOMetaTag.tsx
@@ -1,4 +1,7 @@
 import { Helmet } from 'react-helmet-async';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { isDarkState } from '../../recoil';
 
 interface ISEOMetaTagProps {
   title: string;
@@ -8,25 +11,31 @@ interface ISEOMetaTagProps {
   siteUrl: string;
 }
 const SEOMetaTag = ({ title, description, siteUrl, keywords, img }: ISEOMetaTagProps) => {
+  const isDark = useRecoilValue(isDarkState);
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="robots" content="index,follow" data-react-helmet="true" />
-      <meta name="description" content={description} data-react-helmet="true" />
-      <meta name="keywords" content={keywords} data-react-helmet="true" />
+      <meta name="robots" content="index,follow" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
 
-      <meta property="og:title" content={title} data-react-helmet="true" />
-      <meta property="og:site_name" content={title} data-react-helmet="true" />
-      <meta property="og:description" content={description} data-react-helmet="true" />
-      <meta property="og:type" content="website" data-react-helmet="true" />
-      <meta property="og:image" content={img} data-react-helmet="true" />
-      <meta property="og:url" content={siteUrl} data-react-helmet="true" />
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={img} />
+      <meta property="og:url" content={siteUrl} />
 
-      <meta name="twitter:title" content={title} data-react-helmet="true" />
-      <meta name="twitter:description" content={description} data-react-helmet="true" />
-      <meta name="twitter:image" content={img} data-react-helmet="true" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={img} />
 
-      <link rel="canonical" href={siteUrl} data-react-helmet="true" />
+      <link rel="canonical" href={siteUrl} />
+      {isDark ? (
+        <link href="https://unpkg.com/prism-themes/themes/prism-one-dark.css" rel="stylesheet" />
+      ) : (
+        <link href="https://unpkg.com/prism-themes/themes/prism-one-light.css" rel="stylesheet" />
+      )}
     </Helmet>
   );
 };
