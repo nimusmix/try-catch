953b8bf4a8b8ad551b787c714aadbed894f5f1ad
June <godan6869@naver.com>
2023-02-13 23:36:27 +0900

    fix: 좋아요 수에 따라 ui가 변형되던 문제 수정, 비로그인시 알림버튼이 보이던 문제 수정

--- src/layout/header/NonMemberNavMenu.tsx
+++ src/layout/header/NonMemberNavMenu.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { IconUserCircle } from '../../components/icons/Icons';
 import { isDarkState } from '../../recoil';
 import { API_URL } from '../../constant';
-import NoticeBell from './NoticeBell';
 
 const NonUserWrapper = styled.a`
   padding-right: 1rem;
@@ -14,18 +13,12 @@ const NonUserWrapper = styled.a`
 const NonMemberNavMenu = () => {
   const isDark = useRecoilValue(isDarkState);
   return (
-    <>
-      {/* <Button designType="blueEmpty">회원가입</Button>
-            <Button>로그인</Button> */}
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
-      <NoticeBell />
-      <NonUserWrapper href={`https://${API_URL}/login/oauth2/authorization/github`}>
-        <IconUserCircle
-          color={isDark ? 'var(--colors-white-100)' : 'var(--colors-black-100)'}
-          size="24"
-        />
-      </NonUserWrapper>
-    </>
+    <NonUserWrapper href={`https://${API_URL}/login/oauth2/authorization/github`}>
+      <IconUserCircle
+        color={isDark ? 'var(--colors-white-100)' : 'var(--colors-black-100)'}
+        size="24"
+      />
+    </NonUserWrapper>
   );
 };
 
