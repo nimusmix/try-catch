a8c4a79f6f8c2ba13f5b11380d870df632079bb0
nimusmix <nimusmix@gmail.com>
2023-02-13 16:24:56 +0900

    test: refToken test

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { useSetRecoilState } from 'recoil';
 import { setupInterceptorsTo, tokenInterceptor, TokenRefresher } from './interceptors';
 import { API_URL } from '../constant';
+import isLoggedInState from '../recoil/isLoggedInState';
+import { refToken } from '../recoil/tokenState';
 
 const BASE_URL = `https://${API_URL}/v1`;
 
@@ -16,8 +19,45 @@ const axiosAuthApi = (url: string, options: AxiosRequestConfig = {}) => {
     baseURL: url,
     ...options,
   });
+
+  // 토큰 검증
+  const accToken = JSON.parse(window.sessionStorage.getItem('accToken')!)?.accToken;
+  const TokenCheck = async () => {
+    const setIsLoggedIn = useSetRecoilState(isLoggedInState);
+    const setAccToken = useSetRecoilState(accToken);
+    const setRefToken = useSetRecoilState(refToken);
+
+    const { status } = await axios({
+      method: 'get',
+      url: `${BASE_URL}/token/expired`,
+      headers: {
+        Authorization: accToken,
+      },
+    });
+
+    if (status === 401) {
+      const refToken = JSON.parse(window.localStorage.getItem('refToken')!)?.refToken;
+
+      try {
+        const { headers } = await axios({
+          method: 'get',
+          url: '/token/refresh',
+          headers: { RefreshToken: refToken },
+        });
+
+        const newAccToken = headers.acc;
+        setAccToken(newAccToken);
+      } catch (err) {
+        setIsLoggedIn(false);
+        setAccToken('');
+        setRefToken('');
+        window.location.reload();
+      }
+    }
+  };
+  TokenCheck().then(() => tokenInterceptor(instance));
   // 토큰 주입
-  tokenInterceptor(instance);
+  // tokenInterceptor(instance);
   // TokenRefresher(instance);
   return setupInterceptorsTo(instance);
 };
