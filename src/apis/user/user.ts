05b7e17a1eb5b60202f023b8a011465fe97f1948
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-02-14 08:51:39 +0900

    feat: 기업 구독 기능 추가

--- src/apis/user/user.ts
+++ src/apis/user/user.ts
@@ -1,5 +1,10 @@
-import { api, authApi } from '../../utils/axios-instance';
+import { authApi } from '../../utils/axios-instance';
 
 export const postFollow = (userId: number) => authApi.post(`/user/follow/${userId}`);
 
 export const putFollow = (userId: number) => authApi.put(`/user/follow/${userId}`);
+
+// 기업 구독 (성공 시 200, 실패시 406)
+export const postSubscribe = (companyId: number) => authApi.post(`/user/subscribe/${companyId}`);
+
+export const puttSubscribe = (companyId: number) => authApi.put(`/user/subscribe/${companyId}`);
