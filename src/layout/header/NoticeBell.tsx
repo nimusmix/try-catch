656cf2e47a071020ea25a32972b8a52eb3a0e511
DESKTOP-KVCQHCD\SSAFY <godan6869@naver.com>
2023-02-13 15:01:39 +0900

    feat: 알람 ui 테스트

--- src/layout/header/NoticeBell.tsx
+++ src/layout/header/NoticeBell.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useRecoilState, useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router-dom';
 import { IconBellFill } from '../../components/icons/Icons';
 import { MiniTitle, Paragraph } from '../../components';
 import { isDarkState } from '../../recoil';
 import notificationsState from '../../recoil/notificationsState';
-import elapsedTime from '../../utils/elapsed-time';
+import NoticeItem from './NoticeItem';
 
 const Alert = styled.span``;
 
-const NoticeDropdown = styled.button`
+const NoticeDropdown = styled.button<{ noticeCount: number }>`
   border: none;
   outline: none;
   position: relative;
@@ -17,14 +18,16 @@ const NoticeDropdown = styled.button`
     position: relative;
     width: 0.5rem;
     height: 0.5rem;
-    &:before {
+    ${({ noticeCount }) =>
+      noticeCount > 0 &&
+      `&:before {
       content: '';
       position: absolute;
       width: 0.5rem;
       height: 0.5rem;
       border-radius: var(--borders-radius-round);
       background-color: tomato;
-    }
+    }`}
   }
 `;
 
@@ -98,71 +101,14 @@ const DropUl = styled.ul`
   overflow-y: scroll;
 `;
 
-const DropList = styled.li`
-  position: relative;
-  padding: 0.75rem 0.5rem;
-  margin-right: 0.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
-  border-radius: var(--borders-radius-base);
-  transition: all 0.2s;
-  color: ${({ theme }) => theme.textColor100};
-
-  strong {
-    color: var(--colors-brand-500);
-  }
-
-  .notice-main {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .notice-title {
-    width: 10rem;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    word-break: break-all;
-    margin-right: 0.3rem;
-  }
-
-  .notice-time {
-    font-weight: 600;
-  }
-
-  span {
-    display: flex;
-    align-items: center;
-  }
-
-  svg {
-    margin-right: 0.5rem;
-  }
-
-  &:hover {
-    background-color: ${({ theme: { isDark } }) =>
-      isDark ? 'var(--colors-black-400)' : 'var(--colors-brand-200)'};
-  }
-`;
-
-const Dot = styled.span`
-  width: 0.5rem;
-  height: 0.5rem;
-  border-radius: var(--borders-radius-round);
-  background-color: var(--colors-brand-500);
-  margin-right: 1rem;
-`;
-
 const NoticeBell = () => {
   const isDark = useRecoilValue(isDarkState);
+  const navigate = useNavigate();
   const [notifications, setNotifications] = useRecoilState(notificationsState);
+
   return (
     <div style={{ cursor: 'pointer' }}>
-      <NoticeDropdown>
+      <NoticeDropdown noticeCount={notifications.length}>
         <Alert>
           <IconBellFill
             color={isDark ? 'var(--colors-white-100)' : 'var(--colors-black-100)'}
@@ -185,69 +131,18 @@ const NoticeBell = () => {
             </Paragraph>
           </div>
           <DropUl id="notice-list">
-            <DropList>
-              <div className="notice-main">
-                <span>
-                  <Paragraph sizeType="base" className="notice-title">
-                    가나다라마바사아자차카타파하
-                  </Paragraph>
-                  <p>
-                    님이 회원님을 <strong>팔로우</strong>했어요
-                  </p>
-                </span>
-                <Dot />
-              </div>
-              <Paragraph
-                as="span"
-                sizeType="xm"
-                className="notice-time"
-                color="var(--colors-brand-500)"
-              >
-                {elapsedTime(189239023)}
-              </Paragraph>
-            </DropList>
-            <DropList>
-              <div className="notice-main">
-                <span>
-                  <Paragraph sizeType="base" className="notice-title">
-                    asddasdasasdadasdasdasdsadsad
-                  </Paragraph>
-                  <p>
-                    글에 <strong>답변</strong>이 달렸어요
-                  </p>
-                </span>
-                <Dot />
-              </div>
-              <Paragraph
-                as="span"
-                sizeType="xm"
-                className="notice-time"
-                color="var(--colors-brand-500)"
-              >
-                {elapsedTime(189239023)}
-              </Paragraph>
-            </DropList>
-            <DropList>
-              <div className="notice-main">
-                <span>
-                  <Paragraph sizeType="base" className="notice-title">
-                    asddasdasasdadasdasdasdsadsad
-                  </Paragraph>
-                  <p>
-                    글에 남긴 답변이 <strong>채택</strong>됐어요!
-                  </p>
-                </span>
-                <Dot />
-              </div>
-              <Paragraph
-                as="span"
-                sizeType="xm"
-                className="notice-time"
-                color="var(--colors-brand-500)"
-              >
-                {elapsedTime(189239023)}
-              </Paragraph>
-            </DropList>
+            {notifications.map((notice) => {
+              return (
+                <NoticeItem
+                  key={notice.id}
+                  id={notice.id}
+                  from={notice.from}
+                  title={notice.title}
+                  timestamp={notice.timestamp}
+                  type={notice.type}
+                />
+              );
+            })}
           </DropUl>
         </DropContainer>
       </NoticeDropdown>
