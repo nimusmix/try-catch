b63445b9766ed68169c587ccd6451d16f95aa00b
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-02-15 10:18:10 +0900

    style: loading Spinner  다크모드 색상 변경

--- src/components/loading/LoadingSpinner.tsx
+++ src/components/loading/LoadingSpinner.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
 import { ReactComponent as Spinner } from './spinner.svg';
+import { isDarkState } from '../../recoil';
 
 const Wrapper = styled.div`
   display: flex;
@@ -9,9 +11,12 @@ const Wrapper = styled.div`
   height: 100%;
 `;
 const LoadingSpinner = () => {
+  const isDark = useRecoilValue(isDarkState);
   return (
     <Wrapper>
-      <Spinner />
+      <Spinner
+        style={{ background: isDark ? 'var(--colors-black-500)' : 'var(--colors-white-500)' }}
+      />
     </Wrapper>
   );
 };
